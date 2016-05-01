2f22d834c6312ca3a419959376e43d2874bf3153
tukangdugem <gust4m4n@yahoo.com.sg>
2016-05-02 02:36:16 +0700

    Added: client socket object untuk koneksi ke server.

--- PascalunaClient/frmmain.pas
+++ PascalunaClient/frmmain.pas
@@ -5,10 +5,21 @@ unit frmMain;
 interface
 
 uses
-  Classes, SysUtils, FileUtil, Forms, Controls, Graphics, Dialogs;
+  Classes, SysUtils, FileUtil, lNetComponents, Forms, Controls, Graphics,
+  Dialogs, StdCtrls;
 
 type
-  TForm1 = class(TForm)
+
+  { TFormMain }
+
+  TFormMain = class(TForm)
+    ButtonConnect: TButton;
+    ButtonDisconnect: TButton;
+    ButtonSend: TButton;
+    LTCPComponentClientSocket: TLTCPComponent;
+    procedure ButtonConnectClick(Sender: TObject);
+    procedure ButtonDisconnectClick(Sender: TObject);
+    procedure ButtonSendClick(Sender: TObject);
   private
     { private declarations }
   public
@@ -16,11 +27,28 @@ type
   end;
 
 var
-  Form1: TForm1;
+  FormMain: TFormMain;
 
 implementation
 
 {$R *.lfm}
+
+{ TFormMain }
+
+procedure TFormMain.ButtonConnectClick(Sender: TObject);
+begin
+  LTCPComponentClientSocket.Connect('localhost', 7000);
+end;
+
+procedure TFormMain.ButtonDisconnectClick(Sender: TObject);
+begin
+  LTCPComponentClientSocket.Disconnect;
+end;
+
+procedure TFormMain.ButtonSendClick(Sender: TObject);
+begin
+  LTCPComponentClientSocket.SendMessage('Hello world!');
+end;
 
 end.
 
